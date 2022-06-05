4c829415f248f805b5aa8c555f7fb413c65db7ff
SVA2021 <sofronov.vit@gmail.com>
2022-06-05 21:45:21 +0400

    upd: add test to APP

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,34 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+describe("App", () => {
+	it('render App', () => {
+		render(
+			<Provider store={store}>
+				<App />
+			</Provider>
+		);
+		screen.debug();
+		expect(screen.getByText(/learn/i)).toBeInTheDocument();
+		expect(screen.getByText(/asdhg/i)).toBeInTheDocument();
+		userEvent.click(screen.getByRole('button'));
+		screen.getByRole('textbox');
+	})
+})
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+
+// test('renders learn react link', () => {
+// 	const { getByText } = render(
+// 		<Provider store={store}>
+// 			<App />
+// 		</Provider>
+// 	);
+
+// 	expect(screen.getByText(/learn/i)).toBeInTheDocument();
+
+// 	// render(<App/>);
+// 	screen.debug();
+// });
